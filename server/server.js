df61ae6cd8d4af1c681e739a9048a0da4e9fb70e
Lee-Seungje <moondgod@gmail.com>
2023-11-29 20:27:41 +0900

    Add post

--- server/server.js
+++ server/server.js
@@ -2,8 +2,12 @@ const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config({ path: ".env" });
 const path = require("path");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const next = require("next");
 const { parse } = require("url");
@@ -28,8 +32,13 @@ nextApp
       const { pathname, query } = parsedUrl;
       nextApp.render(req, res, pathname, query);
     });
+
     app.get("*", (req, res) => {
       return handle(req, res);
+    });
+
+    app.post("/test", (req, res, next) => {
+      res.json({ repeat: String(req.body.test.repeat(3)) });
     });
 
     app.listen(port, () => {
--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -5,10 +5,18 @@ import { useState } from "react";
 
 export default function Test() {
   const [hello, setHello] = useState<string>("");
+  const [inputValue, setInputValue] = useState<string>("");
+  const [repeatValue, setRepeatValue] = useState<string>("");
+  const [resValue, setResValue] = useState<string>("");
   const onClick = async () => {
     const res = await axios.get("/api");
 
     setHello(res.data.res);
+  };
+
+  const getRepeat = async () => {
+    const res = await axios.post("/test", { test: "test" });
+    setResValue(res.data.repeat);
   };
 
   return (
@@ -16,6 +24,22 @@ export default function Test() {
       <h1>ㅎㅇ</h1>
       <button onClick={onClick}>click</button>
       <span>{hello}</span>
+      <div>
+        <input
+          value={repeatValue}
+          className="caret-gray-800"
+          onChange={(e) => setRepeatValue(e.target.value)}
+          placeholder="무엇을 반복할까요?"
+        />
+        <input
+          value={inputValue}
+          className="c-caret-gray-800"
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="몇 번 반복할까요?"
+        />
+        <button onClick={getRepeat}>click</button>
+        <h1>{resValue}</h1>
+      </div>
     </>
   );
 }
