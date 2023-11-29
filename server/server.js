32809d8c78ef8b82ac4c9f392a909b1343b751b5
Lee-Seungje <moondgod@gmail.com>
2023-11-29 19:53:33 +0900

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,39 +1,28 @@
 const express = require("express");
 const dotenv = require("dotenv");
-dotenv.config({ path: ".env" }); // 환경변수 사용
+dotenv.config({ path: ".env" });
 const path = require("path");
 
-/** Create Express */
 const app = express();
 
-/** Next.js 모듈 가져오기 */
 const next = require("next");
 const { parse } = require("url");
 
-/** Next.js 설정 */
 const port = process.env.SERVER_PORT;
-/**
- * 개발환경이아니라면 dev 옵션을 false 로 설정하고
- * 서버 시작전에 next build 를 실행해준다.
- */
 const nextApp = next({ dev: true, port });
 const handle = nextApp.getRequestHandler();
 
 nextApp
   .prepare()
   .then(() => {
-    /** Express Settings */
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
-    /** static 경로 설정 */
     app.use(express.static(path.join(__dirname, "../", "public")));
 
-    /** Express Router Settings */
     app.use("/api", (req, res, next) => {
       res.send("hello!");
     });
 
-    /** Next.js Routing */
     app.get("/", (req, res) => {
       const parsedUrl = parse(req.url, true);
       const { pathname, query } = parsedUrl;
